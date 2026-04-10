77a2f18110f7cc8eb293ffc2660f11476445a008
kirankhalid-cs <kiran.khalid1155@gmail.com>
2026-04-10 11:32:12 +0500

    Update ConnectionClass.java

--- ConnectionClass.java
+++ ConnectionClass.java
@@ -22,7 +22,7 @@ public class ConnectionClass {
     }
      public static void main(String[] args)
      {
-//         System.out.println("Classpath: " + System.getProperty("java.class.path"));
+
 
          new   ConnectionClass();
      }
